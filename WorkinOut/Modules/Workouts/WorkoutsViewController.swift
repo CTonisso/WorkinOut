3899730a29e1ab8e6094965335cedccfa39149b6
Carlos Marcelo Tonisso Junior <carlowstonisso@hotmail.com>
2023-05-15 19:25:03 -0300

    Added view controller methods

--- WorkinOut/Modules/Workouts/WorkoutsViewController.swift
+++ WorkinOut/Modules/Workouts/WorkoutsViewController.swift
@@ -12,12 +12,39 @@ class WorkoutsViewController: UIViewController {
     
     private var viewModel: WorkoutsViewModel
     
-    init(_ viewModel: WorkoutsViewModel) {
+    init(viewModel: WorkoutsViewModel) {
         self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = false
+        tabBarController?.tabBar.isHidden = false
+    }
+}
+
+extension WorkoutsViewController: ViewCodable {
+    
+    func buildHierarchy() {
+        
+    }
+    
+    func buildConstraints() {
+        
+    }
+    
+    func setupUI() {
+        
     }
     
 }
